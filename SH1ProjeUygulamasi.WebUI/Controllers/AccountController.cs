12efe01fd3214fdd25235bcf066f46827ff54565
sumeyye <sumeyyekirtil4@gmail.com>
2025-09-18 22:36:16 +0300

    login logout, view component

--- SH1ProjeUygulamasi.WebUI/Controllers/AccountController.cs
+++ SH1ProjeUygulamasi.WebUI/Controllers/AccountController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SH1ProjeUygulamasi.Data;
+using System.Drawing;
+using System.Security.Claims;
 
 namespace SH1ProjeUygulamasi.WebUI.Controllers
 {
 	public class AccountController : Controller
 	{
+		private readonly DatabaseContext _context;
+
+		public AccountController(DatabaseContext context)
+		{
+			_context = context;
+		}
+
 		public IActionResult Index()
 		{
 			return View();
@@ -13,6 +25,39 @@ namespace SH1ProjeUygulamasi.WebUI.Controllers
 		{
 
 			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Login(string email, string password)
+		{
+			// Kullanıcı doğrulama işlemleri burada yapılacak (veritabanı kontrolü)
+			var user = _context.Users.FirstOrDefault(u => u.Email == email && u.Password == password);
+			if (user != null)
+			{
+				// Giriş başarılı, kullanıcıyı yönlendir
+				var haklar = new List<Claim>() //kullanıcı hakları tanımladık
+				{
+					new(ClaimTypes.Email, user.Email), //claim = hak (kullanıcıya tanımlanan haklar)
+						new(ClaimTypes.Role, "Admin")
+				};
+				var kullaniciKimligi = new ClaimsIdentity(haklar, "Login"); //kullanıcı için bir kimlik oluşturduk
+				ClaimsPrincipal claimsPrincipal = new(kullaniciKimligi); //bu sınıftan bir nesne oluşturup bilgilerde saklı haklar ile kural oluşturulabilir
+				HttpContext.SignInAsync(claimsPrincipal); //yukarıdaki yetkilerle sisteme giriş yaptık
+				return RedirectToAction("Index", "Home");
+			}
+			else
+			{
+				// Giriş başarısız, hata mesajı göster
+				//ViewBag.ErrorMessage = "Geçersiz e-posta veya şifre."; //2.yol
+				@TempData["Message"] = "<div class='alert alert-danger'>Giriş Başarısız</div>";
+			}
+			return RedirectToAction("Login", "Account");
+		}
+
+		public ActionResult LogOut() //çıkış yap aktivasyonu : layout
+		{
+			HttpContext.SignOutAsync();
+			return RedirectToAction("Index", "Home");
 		}
 	}
 }
---
+++ SH1ProjeUygulamasi.WebUI/ViewComponents/Category.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SH1ProjeUygulamasi.Data;
+
+namespace SH1ProjeUygulamasi.WebUI.ViewComponents
+{
+	public class Category : ViewComponent
+	{
+		private readonly DatabaseContext _context;
+
+		public Category(DatabaseContext context)
+		{
+			_context = context;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			return View(_context.Categories);
+		}
+	}
+}
