0eb1c87fbef470e7c1454c79562a4a2c14a391a3
sumeyye <sumeyyekirtil4@gmail.com>
2025-09-23 21:22:56 +0300

    created is category and product view

---
+++ SH1ProjeUygulamasi.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SH1ProjeUygulamasi.Data;
+
+namespace SH1ProjeUygulamasi.WebUI.Controllers
+{
+	public class ProductsController : Controller
+	{
+		private readonly DatabaseContext _context;
+
+		public ProductsController(DatabaseContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index(string q = "")
+		{
+			var model = _context.Products.Where(p => p.IsActive && p.Name.Contains(q)).ToList();
+			return View(model);
+		}
+		
+		public IActionResult Detail(int id)
+		{
+			var model = _context.Products.FirstOrDefault(p => p.IsActive && p.Id == id);
+			return View(model);
+		}
+	}
+}
