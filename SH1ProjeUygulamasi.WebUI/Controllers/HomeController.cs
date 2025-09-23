c3e5f5c6e3d412d52c4e5961878c0411975a0007
sumeyye <sumeyyekirtil4@gmail.com>
2025-09-23 22:36:20 +0300

    set is contact us, products, detail view

--- SH1ProjeUygulamasi.WebUI/Controllers/HomeController.cs
+++ SH1ProjeUygulamasi.WebUI/Controllers/HomeController.cs
@@ -31,6 +31,11 @@ namespace SH1ProjeUygulamasi.WebUI.Controllers
         {
             return View();
         }
+        
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
